068155cc0ee2aee548cf38af24a399bec7d96625
peach <ruslana.prus@gmail.com>
2024-05-17 18:32:17 +0200

    hw8

--- HometaskSix/src/examples/inner/math/MathOperation.java
+++
@@ -1,5 +0,0 @@
-package examples.inner.math;
-
-public interface MathOperation {
-    int calc(int a, int b);
-}
--- HometaskSix/src/examples/inner/math/SumOperation.java
+++
@@ -1,8 +0,0 @@
-package examples.inner.math;
-
-public class SumOperation implements MathOperation {
-    @Override
-    public int calc(int a, int b) {
-        return a + b;
-    }
-}
